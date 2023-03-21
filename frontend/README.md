b324d790c9339d0c8ccb80b5747975b396a99d87
jackbisceglia <jackbisceglia2000@gmail.com>
2023-03-21 15:05:54 -0400

    init repo

---
+++ frontend/README.md
@@ -0,0 +1,7 @@
+# Frontend - Activities Finder
+
+Technologies:
+
+- React (js framework): [https://reactjs.org/](https://reactjs.org/), [https://beta.reactjs.org/](https://beta.reactjs.org/)
+- Tailwind (styling): [https://tailwindcss.com/](https://tailwindcss.com/)
+-
---
+++ frontend/src/main.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Route from "./Route";
+
+// App Initialization
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Route />
+  </React.StrictMode>
+);
