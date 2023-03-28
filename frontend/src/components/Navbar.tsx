899b8fdbe62891763a56435f1f0bab2f1356c9fe
Jack Bisceglia <57922742+jackbisceglia@users.noreply.github.com>
2023-03-27 21:46:59 -0400

    init: initialize app again; basic navbar implementation (#1)

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+type PageData = {
+  title: string;
+  href: string;
+};
+
+const pages: PageData[] = [
+  { title: "Home", href: "/" },
+  { title: "Default Search", href: "/results" },
+  { title: "Search", href: "/search" },
+];
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between w-full h-16 text-base font-medium bg-transparent shadow-md shadow-black/5 text-neutral-900 px-28">
+      {/* <nav className="flex justify-between w-full h-16 text-base font-medium text-neutral-900 px-28"> */}
+      {/* TITLE */}
+      <Link
+        href="/"
+        className="my-auto text-xl font-extrabold transition-opacity duration-150 hover:opacity-80"
+      >
+        Activities<span className="font-extrabold text-sky-500">Finder</span>
+      </Link>
+      {/* LINKS */}
+      <ul className="flex gap-4 my-auto">
+        {pages.map(({ href, title }) => (
+          <Link
+            key={title}
+            href={href}
+            className="transition-colors duration-150 hover:text-sky-600 "
+          >
+            <li className="px-2">{title}</li>
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="w-screen min-h-screen p-0 m-0 bg-gradient-to-r from-gray-100 to-gray-300 ">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
