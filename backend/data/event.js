3c013357418c37b251f21b948bdede9f6d15891a
Saniya Sekhon <ssekhon@umass.edu>
2023-03-29 12:59:59 -0400

    Code Style

--- backend/data/event.js
+++ backend/data/event.js
@@ -1,37 +1,28 @@
 class Event {
 
-    constructor(eventId,title, time, location, eventUrl, eventImage, tags){
+    constructor(eventId, title, time, location, eventUrl, eventImage, tags){
         //mandatory
-        this.eventId = eventId
-        this.title = title
-        this.time = time
-        this.location = location
-        this.eventUrl = eventUrl
+        this.eventId = eventId;
+        this.title = title;
+        this.time = time;
+        this.location = location;
+        this.eventUrl = eventUrl;
         //Optional
-        if (tags === undefined) {
-            this.tags = []
-        }
-        else {
-            this.tags = tags
-        }
-        if (eventImage === undefined) {
-            this.eventImage = new Image()
-        }
-        else{
-            this.eventImage = eventImage
-        }
+        this.tags = (tags === undefined)? [] : tags;
+        this.eventImage = (eventImage === undefined)? new Image() : eventImage;
     }
 
-    getTitle(){
-        return this.title
-    }
-    getTime(){
-        return this.time
-    }
-    getLocation() {return this.location}
-    getUrl(){return this.eventUrl}
-    getImage(){return this.image}
+    getTitle = () => this.title;
+
+    getTime = () => this.time;
+    
+    getLocation = () => this.location;
+
+    getUrl = () => this.eventUrl;
+
+    getImage = () => this.image;
 }
 
-let e1 = new Event(0,"Test Event","3:45" , {Town: "Amherst", Building:"Mullins"}, ["basketball", "fun"],"Github.com", undefined, undefined)
+
+let e1 = new Event(0,"Test Event", "3:45" , {Town: "Amherst", Building:"Mullins"}, ["basketball", "fun"],"Github.com", undefined, undefined)
 console.log(e1.getTitle())
