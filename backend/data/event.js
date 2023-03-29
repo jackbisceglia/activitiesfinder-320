118946112d759b79f0783cf4cb79cb9855b89c59
Saniya Sekhon <ssekhon@umass.edu>
2023-03-29 13:14:15 -0400

    Added Setters

--- backend/data/event.js
+++ backend/data/event.js
@@ -2,29 +2,40 @@ class Event {
 
     constructor(eventId, title, time, location, eventUrl, eventImage, tags){
         //mandatory
-        this.eventId = eventId;
-        this.title = title;
-        this.time = time;
-        this.location = location;
-        this.eventUrl = eventUrl;
+        this.eventId = eventId; //number
+        this.title = title; //String
+        this.time = time; //String
+        this.location = location; //Location object (JSON)
+        this.eventUrl = eventUrl; //String
+
         //Optional
-        this.tags = (tags === undefined)? [] : tags;
-        this.eventImage = (eventImage === undefined)? new Image() : eventImage;
+        this.tags = (tags === undefined)? [] : tags; //array
+        this.eventImage = (eventImage === undefined)? new Image() : eventImage; //Image object (JSON)
     }
 
+    getEventId = () => this.eventId;
+    setEventId = (newId) => {this.eventId = newId};
+
     getTitle = () => this.title;
+    setTitle = (newTitle) => {this.title = newTitle};
 
     getTime = () => this.time;
+    setTime = (newTime) => {this.time = newTime};
     
     getLocation = () => this.location;
+    setLocation = (newLocation) => {this.location = newLocation};
 
     getUrl = () => this.eventUrl;
+    setUrl = (newUrl) => {this.Url = newUrl};
 
     getImage = () => this.image;
+    setImage = (newImg) => {this.image = newImg};
 }
 
-
 let e1 = new Event(0,"Test Event", "3:45" , {Town: "Amherst", Building:"Mullins"}, ["basketball", "fun"],"Github.com", undefined, undefined);
+console.log(e1.getEventId());
+e1.setEventId(11);
+console.log(e1.getEventId());
 console.log(e1.getTitle());
 console.log(e1.getTime());
 console.log(e1.getLocation());
