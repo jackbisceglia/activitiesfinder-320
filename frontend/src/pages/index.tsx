e26307f97435407daab095d29ccd945273d5d8fe
Jack Bisceglia <57922742+jackbisceglia@users.noreply.github.com>
2023-04-06 14:54:31 -0400

    feat: implement event loading from local storage & restyle event cards (#5)
    * feat: EventCard implementation
    
    * feat: event card styles; load events from local storage

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,126 +1,99 @@
-import { useEffect, useState } from "react";
+import { EventCard, GenericEvent } from "@/components/EventCard";
+import { useEffect, useReducer, useState } from "react";
 
 import Link from "next/link";
 
-type Event = {
-  id: number;
-  title: string;
-  location: string;
-  // change to Date type
-  date: string;
-  time: string;
+export type EventActions =
+  | { type: "REMOVE"; payload: { id: string } }
+  | { type: "ADD"; payload: { event: GenericEvent } }
+  | { type: "LOAD_ALL" };
+
+const eventReducer = (
+  events: GenericEvent[],
+  action: EventActions
+): GenericEvent[] => {
+  switch (action.type) {
+    case "REMOVE":
+      const filteredEvents = events.filter((e) => e.id !== action.payload.id);
+      localStorage.setItem("events", JSON.stringify(filteredEvents));
+      return filteredEvents;
+    case "ADD":
+      const newEvents = [...events, action.payload.event];
+      localStorage.setItem("events", JSON.stringify(newEvents));
+      return newEvents;
+    case "LOAD_ALL":
+      return JSON.parse(localStorage.getItem("events") ?? "[]");
+    default:
+      console.log("default");
+      return events;
+  }
 };
 
-const mockEvents = [
-  {
-    id: 1,
-    title: "Event 1",
-    location: "Event 1 location",
-    date: "Event 1 date",
-    time: "Event 1 time",
-  },
-  {
-    id: 2,
-    title: "Event 2",
-    location: "Event 2 location",
-    date: "Event 2 date",
-    time: "Event 2 time",
-  },
-  {
-    id: 3,
-    title: "Event 3",
-    location: "Event 3 location",
-    date: "Event 3 date",
-    time: "Event 3 time",
-  },
-  {
-    id: 4,
-    title: "Event 4",
-    location: "Event 4 location",
-    date: "Event 4 date",
-    time: "Event 4 time",
-  },
-  {
-    id: 5,
-    title: "Event 5",
-    location: "Event 5 location",
-    date: "Event 5 date",
-    time: "Event 5 time",
-  },
-];
-
-const useEvents = () => {
-  const [events, setEvents] = useState<Event[]>([]);
+// Event State
+const useEventState = () => {
+  const defaultState: GenericEvent[] = [];
+  const [events, dispatchEvents] = useReducer(eventReducer, defaultState);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    // WILL FETCH FROM LOCAL STORAGE IN FUTURE
     const fetchEvents = async () => {
       setLoading(true);
-      setEvents(() => mockEvents);
-      // setEvents(() => []);
+      dispatchEvents({
+        type: "LOAD_ALL",
+      });
       setLoading(false);
     };
 
     fetchEvents();
   }, []);
 
-  return { events, loading, error };
+  return { events, loading, error, eventStateDispatch: dispatchEvents };
 };
 
 const FindEventsPanel = ({ hasEvents }: { hasEvents: boolean }) => {
-  const buttonText = hasEvents ? "Find More Events" : "Find Events";
-
   const fullScreenStyle =
-    " justify-center items-center my-auto rounded-md bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-sky-400 to-indigo-900 flex w-full min-h-[25rem]";
+    " justify-center flex-grow items-center my-auto rounded-md bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-sky-400 to-indigo-900 flex min-h-[30rem]";
   const singleColumnStyle =
     "flex items-center justify-center rounded-md bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-sky-400 to-indigo-900 w-72";
 
-  const divStyle = hasEvents ? singleColumnStyle : fullScreenStyle;
-
   return (
-    <div className={divStyle}>
+    <div className={hasEvents ? singleColumnStyle : fullScreenStyle}>
       <Link
         href="results"
         className="px-6 py-3 font-medium transition-colors duration-300 rounded-md shadow-md bg-sky-100 hover:bg-sky-50"
       >
-        {buttonText}
+        {hasEvents ? "Find More Events" : "Find Events"}
       </Link>
-    </div>
-  );
-};
-
-const EventsList = ({ events }: { events: Event[] }) => {
-  return (
-    <div className="flex flex-col flex-1 w-auto gap-4">
-      {events.map((e) => (
-        <div
-          className="flex flex-col px-6 py-4 bg-gray-600 rounded-md text-neutral-300"
-          key={e.id}
-        >
-          <p className="text-xl font-black">{e.title}</p>
-          <p>{e.location}</p>
-          <p>{e.date}</p>
-          <p>{e.time}</p>
-        </div>
-      ))}
     </div>
   );
 };
 
 // HOMEPAGE
 export default function Home() {
-  const { events, loading, error } = useEvents();
+  const { events, loading, error, eventStateDispatch } = useEventState();
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
 
+  const EventList = () => (
+    <div className="flex flex-col flex-1 w-auto gap-4">
+      {events.map((e) => (
+        <EventCard
+          key={e.id}
+          event={e}
+          eventStateDispatch={eventStateDispatch}
+          eventSaved={true}
+        />
+      ))}
+    </div>
+  );
+
   return (
-    <div className="flex flex-col gap-4 py-12 sm:flex-row ">
+    <div className="flex flex-col h-full gap-6 py-12 sm:flex-row ">
       {events.length ? (
         <>
-          <EventsList events={events} />
+          <EventList />
           <FindEventsPanel hasEvents={true} />
         </>
       ) : (
