1dfb2afcdafa7fa74a961d4293b8fd3d56595460
jackbisceglia <jackbisceglia2000@gmail.com>
2023-04-26 11:22:20 -0400

    feat: frontend clerk implementation

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,21 +1,60 @@
 import "@/styles/globals.css";
 
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+  useAuth,
+  useUser,
+} from "@clerk/nextjs";
+import { Plus_Jakarta_Sans, Tourney } from "next/font/google";
+
 import type { AppProps } from "next/app";
-import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "../components/Navbar";
-import { Plus_Jakarta_Sans } from "next/font/google";
+import React from "react";
+import { useRouter } from "next/router";
 
 const font = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
+const RedirectToLandingPage = () => {
+  const router = useRouter();
+
+  router.replace("/landing");
+
+  return null;
+};
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const publicPages = ["landing", "signin", "signup"];
+
+  const publicPagePaths = publicPages.map((p) => `/${p}`);
+
+  const Page = () => (
+    <div className={`${font.className} min-h-screen flex flex-col`}>
+      <Navbar />
+      <main className="flex flex-1 w-full max-w-screen-lg min-h-full px-8 mx-auto text-neutral-900">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
+
   return (
     <ClerkProvider {...pageProps}>
-      <div className={`${font.className} min-h-screen flex flex-col`}>
-        <Navbar />
-        <main className="w-full max-w-screen-lg px-8 mx-auto text-neutral-900">
-          <Component {...pageProps} />
-        </main>
-      </div>
+      {publicPagePaths.includes(router.pathname) ? (
+        <Page />
+      ) : (
+        <>
+          <SignedIn>
+            <Page />
+          </SignedIn>
+          <SignedOut>
+            <RedirectToLandingPage />
+          </SignedOut>
+        </>
+      )}
     </ClerkProvider>
   );
 }
--- frontend/src/pages/testclerk.tsx
+++
@@ -1,7 +0,0 @@
-export default function TestClerk() {
-  return (
-    <div>
-      <h1>Testing Clerk</h1>
-    </div>
-  );
-}
