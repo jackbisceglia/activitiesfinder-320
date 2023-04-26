e7f64fd8e486bd776b70ee98d5d21c89b5c0e89b
Saniya Sekhon <ssekhon@umass.edu>
2023-04-26 14:13:14 -0400

    Able to read .env file for ApiKey

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,23 @@
 import cors from "cors";
 // import userRouter from "./routes/users.js";
 import eventRouter from "./routes/callEvents.js";
+
+// import "./config.env"; // To read CLERK_API_KEY
+// require('dotenv').config({ path: './config.env' });
+
+import { config } from 'dotenv';
+config({ path: './config.env' });
+import { ClerkExpressWithAuth } from "@clerk/clerk-sdk-node";
+
 import express from "express";
 
 // init app
+const port = process.env.PORT || 8080;
 const app = express();
-const port = 8080;
+
+
+const apiKey = process.env.CLERK_SECRET_KEY;
+console.log("Key: " , apiKey);
 
 // middleware
 app.use(cors());
@@ -16,10 +28,20 @@ app.use(cors());
 // app.use("/user", userRouter);
 
 // test route: GET request to http://localhost:8080/
-
 app.get("/", async (req, res) => {
   res.send("Hello World!");
 });
+
+// Use the lax middleware that returns an empty auth object when unauthenticated
+app.get(
+  "/protected-endpoint",
+  ClerkExpressWithAuth({
+    // ...options
+  }),
+  (req, res) => {
+    res.json(req.auth);
+  }
+);
 
 app.use("/events", eventRouter);
 
