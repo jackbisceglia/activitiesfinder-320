3167a41d6677a1bb80f11fdb2c87ee79101da38e
Elizabeth <elizabeth@ElizabethdeMacBook-Air.local>
2023-04-30 12:27:48 -0400

    preferencesURL

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home() {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
-
+  console.log(events)
   const EventList = () => (
     <div className="flex flex-col flex-1 w-auto gap-4">
       {events.map((e) => (
